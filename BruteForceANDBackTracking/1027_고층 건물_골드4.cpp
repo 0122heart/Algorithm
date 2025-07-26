5784bcdbbd706c5885f3e5aa6cbd64bdef848e96
0122heart <0122heart@gmail.com>
2025-07-26 10:20:38 +0900

    Update 1027_고층 건물_골드4.cpp

--- BruteForceANDBackTracking/1027_고층 건물_골드4.cpp
+++ BruteForceANDBackTracking/1027_고층 건물_골드4.cpp
@@ -8,49 +8,41 @@ int main()
     cin >> num_building;
     for(int i = 0; i < num_building; i++) cin >> height[i];
 
-    double inclination[50][50];
-    // 기준 빌딩이 i
-    for(int i = 0; i < num_building; i++){
-        // 비교 빌딩이 j
-        for(int j = 0; j < i; j++){
-            inclination[i][j] = 1.0 * (height[i] - height[j]) / (i - j);
-        }
-        for(int j = i + 1; j < num_building; j++){
-            inclination[i][j] = 1.0 * (height[j] - height[i]) / (j - i);
-        }
-    }
-
     int result = 0;
     for(int i = 0; i < num_building; i++){
         int cnt = 0;
 
         // 바로 왼쪽의 경우
-        double standard = DBL_MAX;
+        int p, q;
 
         if(0 <= i - 1){
             cnt++;
-            standard = inclination[i][i - 1];
+            p = height[i] - height[i - 1];
+            q = 1;
         }
 
         for(int j = i - 2; -1 < j; j--){
             // 기울기가 감소하면 cnt ++
-            if(inclination[i][j] < standard){
+            if((long long)q * (height[i] - height[j]) < (long long)p * (i - j)){
                 cnt++;
-                standard = inclination[i][j];
+                p = height[i] - height[j];
+                q = i - j;
             }
         }
 
         // 바로 오른쪽의 경우
-        standard = DBL_MIN;
         if(i + 1 < num_building){
             cnt++;
-            standard = inclination[i][i + 1];
+            p = height[i + 1] - height[i];
+            q = 1;
         }
 
         for(int j = i + 1; j < num_building; j++){
-            if(standard < inclination[i][j]){
+            // 기울기가 증가하면 cnt++
+            if((long long)p * (j - i) < (long long)q * (height[j] - height[i])){
                 cnt++;
-                standard = inclination[i][j];
+                p = height[j] - height[i];
+                q = j - i;
             }
         }
 
